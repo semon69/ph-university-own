5ce9432e79a6988f58db8f0ef2123eb140105a6b
semon69 <smdemon3@gmail.com>
2023-11-17 19:55:12 +0600

    setup mongoose, eslint, prettier project using express

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+import express, { Request, Response } from 'express';
+import cors from 'cors';
+const app = express();
+
+// Parser
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World!');
+});
+
+export default app;
