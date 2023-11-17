81a6a04468b53dd5c471e2700d932745c33fb9d8
semon69 <smdemon3@gmail.com>
2023-11-17 21:15:41 +0600

    create student interface and Schema model

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,7 @@ app.use(express.json());
 app.use(cors());
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!');
+  res.send('Hello from World!');
 });
 
 export default app;
--- src/server.ts
+++ src/server.ts
@@ -6,7 +6,7 @@ async function main() {
   try {
     await mongoose.connect(config.database_url as string);
     app.listen(config.port, () => {
-      console.log(`Example app listening on port ${config.port}`);
+      console.log(`App listening on port ${config.port}`);
     });
   } catch (error) {
     console.log(error);
